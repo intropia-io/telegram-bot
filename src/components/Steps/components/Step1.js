d5c72b11c0461b481fc03a6fac0145b322a68e5b
VladyPoim <unreal0man@gmail.com>
2022-11-22 12:00:09 +0300

    update

--- src/components/Steps/components/Step1.js
+++ src/components/Steps/components/Step1.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { useStepData, useSetStep, maxStageLength } from '../../../state/stepState';
 
@@ -8,10 +8,16 @@ import { useTelegram } from '../../../hooks/useTelegram';
 
 const Step1 = () => {
 
+    const line1 = "Hey:) I’m Spike, tr3butor assistant.";
+    const line2 = "I’ll help you to customize web3 opportunity feed";
+
+    const [speechlines, setSpeechlines] = useState([line1]);
+
     const { tg, onToggleButton } = useTelegram();
 
     const stage = useStepData();
     const setStage = useSetStep();
+
   
     const nextStage = useCallback(() => {
       if (stage < maxStageLength) {
@@ -19,35 +25,36 @@ const Step1 = () => {
       }}, [stage, setStage]);
 
     useEffect(() => {
-        tg.MainButton.setParams({
-            text: "NEXT"
-        })
-    }, [tg.MainButton])
+        const timeout = setTimeout(() => {
+            tg.MainButton.setParams({
+                text: "NEXT"
+            })
+            setSpeechlines(prev => [...prev, line2])
+        }, 2000)
+
+        return () => clearTimeout(timeout);
+    }, [tg.MainButton, setSpeechlines])
 
     useEffect(() => {
         onToggleButton()
     }, [onToggleButton])
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            tg.onEvent('mainButtonClicked', nextStage)
-        }, 2000)
-        return () => clearTimeout(timeout);
+        tg.onEvent('mainButtonClicked', nextStage)
+        return () => {
+            tg.offEvent('mainButtonClicked', nextStage)
+        }
     }, [nextStage, tg])
 
     return (
         <ModalContainer>
-            <div>
+            {speechlines.map((speechline, index) => 
+            <div key={index}>
                 <p>
-                    Hey:) I’m Spike, tr3butor assistant.
+                   {speechline}
                 </p>
             </div>
-
-            <div>
-                <p>
-                    I’ll help you to customize web3 opportunity feed
-                </p>
-            </div>
+            )}
         </ModalContainer>
     );
 };
